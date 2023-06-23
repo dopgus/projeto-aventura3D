ade98f72bd69658c5ad52f4bcf5b4899df41d654
dopgus <gustavolimadacruz4@gmail.com>
2023-06-23 10:13:00 -0300

    finalizando e polindo o game

--- Aventura3D/Assets/Inputs/Inputs.cs
+++ Aventura3D/Assets/Inputs/Inputs.cs
@@ -25,6 +25,22 @@ public class @Inputs : IInputActionCollection, IDisposable
                     ""expectedControlType"": ""Button"",
                     ""processors"": """",
                     ""interactions"": """"
+                },
+                {
+                    ""name"": ""Weapon1"",
+                    ""type"": ""Button"",
+                    ""id"": ""33c9ce47-7e43-4ee3-a8fa-7146ff489c78"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Weapon2"",
+                    ""type"": ""Button"",
+                    ""id"": ""dd03e736-1850-4bf7-ae5f-f578a4026e4c"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
                 }
             ],
             ""bindings"": [
@@ -38,6 +54,28 @@ public class @Inputs : IInputActionCollection, IDisposable
                     ""action"": ""Shoot"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""dca2896d-8b5d-4ee1-b662-8f783b456e9f"",
+                    ""path"": ""<Keyboard>/1"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Weapon1"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""4028995b-c701-4771-abd2-08c0a7056ae5"",
+                    ""path"": ""<Keyboard>/2"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Weapon2"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -47,6 +85,8 @@ public class @Inputs : IInputActionCollection, IDisposable
         // Gameplay
         m_Gameplay = asset.FindActionMap("Gameplay", throwIfNotFound: true);
         m_Gameplay_Shoot = m_Gameplay.FindAction("Shoot", throwIfNotFound: true);
+        m_Gameplay_Weapon1 = m_Gameplay.FindAction("Weapon1", throwIfNotFound: true);
+        m_Gameplay_Weapon2 = m_Gameplay.FindAction("Weapon2", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -97,11 +137,15 @@ public class @Inputs : IInputActionCollection, IDisposable
     private readonly InputActionMap m_Gameplay;
     private IGameplayActions m_GameplayActionsCallbackInterface;
     private readonly InputAction m_Gameplay_Shoot;
+    private readonly InputAction m_Gameplay_Weapon1;
+    private readonly InputAction m_Gameplay_Weapon2;
     public struct GameplayActions
     {
         private @Inputs m_Wrapper;
         public GameplayActions(@Inputs wrapper) { m_Wrapper = wrapper; }
         public InputAction @Shoot => m_Wrapper.m_Gameplay_Shoot;
+        public InputAction @Weapon1 => m_Wrapper.m_Gameplay_Weapon1;
+        public InputAction @Weapon2 => m_Wrapper.m_Gameplay_Weapon2;
         public InputActionMap Get() { return m_Wrapper.m_Gameplay; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -114,6 +158,12 @@ public class @Inputs : IInputActionCollection, IDisposable
                 @Shoot.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnShoot;
                 @Shoot.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnShoot;
                 @Shoot.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnShoot;
+                @Weapon1.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnWeapon1;
+                @Weapon1.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnWeapon1;
+                @Weapon1.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnWeapon1;
+                @Weapon2.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnWeapon2;
+                @Weapon2.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnWeapon2;
+                @Weapon2.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnWeapon2;
             }
             m_Wrapper.m_GameplayActionsCallbackInterface = instance;
             if (instance != null)
@@ -121,6 +171,12 @@ public class @Inputs : IInputActionCollection, IDisposable
                 @Shoot.started += instance.OnShoot;
                 @Shoot.performed += instance.OnShoot;
                 @Shoot.canceled += instance.OnShoot;
+                @Weapon1.started += instance.OnWeapon1;
+                @Weapon1.performed += instance.OnWeapon1;
+                @Weapon1.canceled += instance.OnWeapon1;
+                @Weapon2.started += instance.OnWeapon2;
+                @Weapon2.performed += instance.OnWeapon2;
+                @Weapon2.canceled += instance.OnWeapon2;
             }
         }
     }
@@ -128,5 +184,7 @@ public class @Inputs : IInputActionCollection, IDisposable
     public interface IGameplayActions
     {
         void OnShoot(InputAction.CallbackContext context);
+        void OnWeapon1(InputAction.CallbackContext context);
+        void OnWeapon2(InputAction.CallbackContext context);
     }
 }
--- Aventura3D/Assets/Scripts/Player/PlayerAbilityShoot.cs
+++ Aventura3D/Assets/Scripts/Player/PlayerAbilityShoot.cs
@@ -7,10 +7,13 @@ public class PlayerAbilityShoot : PlayerAbilityBase
 {
     public List<UIGunUpdater> uIGunUpdaters;
 
-    public GunBase gunBase;
+    public GunBase gunShootLimit;
+    public GunBase gunShootAngle;
     public Transform gunPosition;
 
     private GunBase _currentGun;
+    private GunBase gunShootLimitInstance;
+    private GunBase gunShootAngleInstance;
 
     protected override void Init()
     {
@@ -20,13 +23,39 @@ public class PlayerAbilityShoot : PlayerAbilityBase
 
         inputs.Gameplay.Shoot.performed += ctx => StartShoot();
         inputs.Gameplay.Shoot.canceled += ctx => CancelShoot();
+
+        inputs.Gameplay.Weapon1.performed += ctx => SetWeaponActive(1);
+        inputs.Gameplay.Weapon2.performed += ctx => SetWeaponActive(2);
     }
 
     private void CreateGun()
     {
-        _currentGun = Instantiate(gunBase, gunPosition);
+        gunShootLimitInstance = Instantiate(gunShootLimit, gunPosition);
+        gunShootLimitInstance.transform.localPosition = gunShootLimitInstance.transform.localEulerAngles = Vector3.zero;
 
-        _currentGun.transform.localPosition = _currentGun.transform.localEulerAngles = Vector3.zero;
+        gunShootAngleInstance = Instantiate(gunShootAngle, gunPosition);
+        gunShootAngleInstance.transform.localPosition = gunShootAngleInstance.transform.localEulerAngles = Vector3.zero;
+        gunShootAngleInstance.gameObject.SetActive(false);
+
+        _currentGun = gunShootLimitInstance;
+    }
+
+    private void SetWeaponActive(int weaponActiveNumber)
+    {
+        _currentGun.gameObject.SetActive(false);
+        switch (weaponActiveNumber)
+        {
+            case 1:
+                _currentGun = gunShootLimitInstance;
+                break;
+            case 2:
+                _currentGun = gunShootAngleInstance;
+                break;
+            default:
+                Debug.LogError("We don't have the given weapon number: " + weaponActiveNumber);
+                break;
+        }
+        _currentGun.gameObject.SetActive(true);
     }
 
     private void StartShoot()
