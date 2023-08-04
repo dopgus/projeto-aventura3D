d7f752524f68dff9e242feb37f0c0520ecd6c4c4
dopgus <gustavolimadacruz4@gmail.com>
2023-08-04 11:28:17 -0300

    adicionando particulas

--- Aventura3D/Assets/Scripts/Enemies/EnemyBase.cs
+++ Aventura3D/Assets/Scripts/Enemies/EnemyBase.cs
@@ -10,6 +10,8 @@ namespace Enemy
     public class EnemyBase : MonoBehaviour, IDamageable
     {
         public Collider collider;
+        public FlashColor flashColor;
+        public ParticleSystem particleSystem;
 
         public float startLife = 10f;
 
@@ -54,6 +56,9 @@ namespace Enemy
 
         public void OnDamage(float f)
         {
+            if (flashColor != null) flashColor.Flash();
+            if (particleSystem != null) particleSystem.Emit(15);
+
             _currentLife -= f;
 
             if(_currentLife <= 0)
