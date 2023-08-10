ab9362b859a7077279a8a8bd6dda0a865361ba3e
dopgus <gustavolimadacruz4@gmail.com>
2023-08-10 11:50:12 -0300

    desenvolvendo o boss pt.2

--- Aventura3D/Assets/Scripts/Boss/BossStates.cs
+++ Aventura3D/Assets/Scripts/Boss/BossStates.cs
@@ -30,7 +30,50 @@ namespace Boss
         public override void OnStateEnter(params object[] objs)
         {
             base.OnStateEnter(objs);
-            boss.GoToRandomPoint();
+            boss.GoToRandomPoint(OnArrive);
+        }
+
+        private void OnArrive()
+        {
+            boss.SwitchState(BossAction.ATTACK);
+        }
+
+        public override void OnStateExit()
+        {
+            base.OnStateExit();
+            Debug.Log("Exit Walk");
+            boss.StopAllCoroutines();
+        }
+    }
+
+    public class BossStateAttack : BossStateBase
+    {
+        public override void OnStateEnter(params object[] objs)
+        {
+            base.OnStateEnter(objs);
+            boss.StartAttack(EndAttacks);
+        }
+
+        private void EndAttacks()
+        {
+            boss.SwitchState(BossAction.WALK);
+        }
+
+        public override void OnStateExit()
+        {
+            Debug.Log("Exit Attack");
+            base.OnStateExit();
+            boss.StopAllCoroutines();
+        }
+    }
+
+    public class BossStateDeath : BossStateBase
+    {
+        public override void OnStateEnter(params object[] objs)
+        {
+            base.OnStateEnter(objs);
+            Debug.Log("Enter Death");
+            boss.transform.localScale = Vector3.one * .2f;
         }
     }
 }
