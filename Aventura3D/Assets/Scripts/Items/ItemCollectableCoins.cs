9327959e62f58665194a2ae76e190245302ba815
dopgus <gustavolimadacruz4@gmail.com>
2023-09-05 10:29:06 -0300

    criando um inventario

--- Aventura3D/Assets/Scripts/Items/ItemCollectableCoins.cs
+++ Aventura3D/Assets/Scripts/Items/ItemCollectableCoins.cs
@@ -2,33 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Itens;
 
 public class ItemCollectableCoins : CollectableBase
 {
-    public static ItemCollectableCoins instance;
-    public TextMeshProUGUI text;
-    int score;
     public Collider2D collider;
-
-    void Start()
-    {
-        if(instance == null)
-        {
-            instance = this;
-        }
-    }
 
     protected override void OnCollect()
     {
         base.OnCollect();
-        ItemManager.Instance.AddCoins();
+        ItemManager.Instance.AddByType(ItemType.COIN);
         collider.enabled = false;
-    }
-
-    public void ChangeScore(int coinValue)
-    {
-        score += coinValue;
-        text.text = "X" + score.ToString();
     }
 
 }
