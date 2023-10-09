0f3f32ba68dcb5542d48f36521f19b80b969295d
dopgus <gustavolimadacruz4@gmail.com>
2023-10-09 10:28:42 -0300

    arrumando alguns bugs

--- Aventura3D/Assets/Scripts/Items/ItemManager.cs
+++ Aventura3D/Assets/Scripts/Items/ItemManager.cs
@@ -24,8 +24,8 @@ namespace Items
 
         private void LoadItemsFromSave()
         {
-            AddByType(ItemType.COIN, (int)SaveManager.Instance.Setup.coins);
-            AddByType(ItemType.LIFE_PACK, (int)SaveManager.Instance.Setup.health);
+            AddByType(ItemType.COIN, (int) SaveManager.Instance.Setup.coins);
+            AddByType(ItemType.LIFE_PACK, (int) SaveManager.Instance.Setup.health);
         }
 
         private void Reset()
